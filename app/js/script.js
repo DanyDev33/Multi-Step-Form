80936dd2f99ef7f18076b24498bc75c47b7cf99d
Daniel Hedima <shamzy647@gmail.com>
2025-06-05 14:11:15 +0100

    Code Updates

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,6 @@
 const stepOne = document.getElementById('step__one').hidden = true
-const stepTwo = document.getElementById('step__two').hidden = true
+const stepTwo = document.getElementById('step__two').hidden = false
 const stepThree = document.getElementById('step__three').hidden = true
 const stepFour = document.getElementById('step__four').hidden = true
 const stepFive = document.getElementById('step__five').hidden = true
 
-console.log(stepFour.hidden)
