6325129bf001a34a139ede32c9f795c353b1c152
Daniel Hedima <shamzy647@gmail.com>
2025-06-06 15:28:26 +0100

    CSS Update

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
-const stepOne = document.getElementById('step__one').hidden = true
+const stepOne = document.getElementById('step__one').hidden = false
 const stepTwo = document.getElementById('step__two').hidden = true
 const stepThree = document.getElementById('step__three').hidden = true
-const stepFour = document.getElementById('step__four').hidden = false
+const stepFour = document.getElementById('step__four').hidden = true
 const stepFive = document.getElementById('step__five').hidden = true
 
