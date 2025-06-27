cf03f41e8c4f9427a513652d531ac85bc3953327
Daniel Hedima <shamzy647@gmail.com>
2025-06-27 18:06:23 +0100

    HTML and JavaScript Update

--- app/js/script.js
+++ app/js/script.js
@@ -1,128 +1,11 @@
 /* VARIABLES */
-
-/* Steps (1-5) */
-/* const stepOne = document.getElementById('step__one')
-const stepTwo = document.getElementById('step__two')
-const stepThree = document.getElementById('step__three')
-const stepFour = document.getElementById('step__four')
-const stepFive = document.getElementById('step__five') */
-/* Selected Steps */
-/* const selectedStepsOne = document.getElementById('selected-steps-one')
-const selectedStepsTwo = document.getElementById('selected-steps-two')
-const selectedStepsThree = document.getElementById('selected-steps-three')
-const selectedStepsFour = document.getElementById('selected-steps-four') */
-/* Next Step Buttons */
-/* const stepOneBtn = document.getElementById('step-one-btn')
-const stepTwoBtn = document.getElementById('step-two-btn')
-const stepThreeBtn = document.getElementById('step-three-btn')
-const stepFourBtn = document.getElementById('step-four-btn') */
-/* Previous Step Buttons */
-/* const changePlan = document.getElementById('change-plan')
-const previousStepTwoBtn = document.getElementById('back-two-btn')
-const previousStepThreeBtn = document.getElementById('back-three-btn')
-const previousStepFourBtn = document.getElementById('back-four-btn') */
-/* Input Fields (Step 1) */
-/* const nameInput = document.getElementById('name')
-const emailInput = document.getElementById('email-address')
-const numberInput = document.getElementById('phone-number') */
-
-/* EVENT LISTENERS */
-/* stepOneBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        stepOne.classList.add('hidden')
-        stepTwo.classList.remove('hidden')
-        selectedStepsOne.classList.remove('selected-steps')
-        selectedStepsTwo.classList.add('selected-steps')
-    }, 1000)
-
-})
-
-stepTwoBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        stepTwo.classList.add('hidden')
-        stepThree.classList.remove('hidden')
-        selectedStepsTwo.classList.remove('selected-steps')
-        selectedStepsThree.classList.add('selected-steps')
-    }, 1000)
-})
-
-stepThreeBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        stepThree.classList.add('hidden')
-        stepFour.classList.remove('hidden')
-        selectedStepsThree.classList.remove('selected-steps')
-        selectedStepsFour.classList.add('selected-steps')
-    }, 1000);
-})
-
-stepFourBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        stepFour.classList.add('hidden')
-        stepFive.classList.remove('hidden')
-    }, 1000);
-})
-
-previousStepTwoBtn.addEventListener('click', () => {
-    stepTwo.classList.add('hidden')
-    stepOne.classList.remove('hidden')
-    selectedStepsTwo.classList.remove('selected-steps')
-    selectedStepsOne.classList.add('selected-steps')
-})
-
-previousStepThreeBtn.addEventListener('click', () => {
-    stepThree.classList.add('hidden')
-    stepTwo.classList.remove('hidden')
-    selectedStepsThree.classList.remove('selected-steps')
-    selectedStepsTwo.classList.add('selected-steps')
-})
-
-previousStepFourBtn.addEventListener('click', () => {
-    stepFour.classList.add('hidden')
-    stepThree.classList.remove('hidden')
-    selectedStepsFour.classList.remove('selected-steps')
-    selectedStepsThree.classList.add('selected-steps')
-}) */
 const steps = document.querySelectorAll('.steps')
 const stepsBtn = document.querySelectorAll('.steps-btn')
-
-/* stepsBtn.forEach(stepBtn => {
-    stepBtn.addEventListener('click', () => {
-        steps.forEach(step => {
-            if (step.classList.contains('hidden')) {
-                step.classList.add('hidden')
-                step.nextElementSibling.classList.remove('hidden')
-            }
-        });
-    })
-}) */
-
-stepsBtn.forEach((stepBtn, index) => {
-    stepBtn.addEventListener('click', () => {
-        setTimeout(() => {
-            // Hide current step
-            steps[index].classList.add('hidden');
-
-            // Show the next step if it exists
-            if (steps[index + 1]) {
-                steps[index + 1].classList.remove('hidden');
-            }
-        }, 1000)
-    })
-})
+const stepsSelected = document.querySelectorAll('.sidebar__circle')
+const lastStepsBtn = document.querySelectorAll('.last-steps-btn')
 
 const plansSelected = document.querySelectorAll('.plan__monthly-cards')
 
-plansSelected.forEach(planSelected => {
-    planSelected.addEventListener('click', () => {
-        plansSelected.forEach(planSel => {
-            planSel.classList.remove('card-selected')
-        })
-        planSelected.classList.add('card-selected')
-
-    })
-});
-
-/* Monthly/Yearly Toggle */
 const toggleInput = document.querySelector('.toggle-input')
 const plans = document.querySelectorAll('.subscription__text')
 const extraMonths = document.querySelectorAll('.plan-text3')
@@ -134,8 +17,51 @@ const addOnsPrice1 = document.getElementById('add-ons-one')
 const addOnsPrice2 = document.getElementById('add-ons-two')
 const addOnsPrice3 = document.getElementById('add-ons-three')
 
+const addOnsSelected = document.querySelectorAll('.input__checkbox')
 
+/* Next Step */
+stepsBtn.forEach((stepBtn, index) => {
+    stepBtn.addEventListener('click', () => {
+        setTimeout(() => {
+            // Hide current step
+            steps[index].classList.add('hidden')
+            stepsSelected[index].classList.remove('selected-steps')
 
+            // Show the next step if it exists
+            if (steps[index + 1]) {
+                steps[index + 1].classList.remove('hidden');
+                stepsSelected[index + 1].classList.add('selected-steps')
+            }
+        }, 1000)
+    })
+})
+
+/* Previous Step */
+lastStepsBtn.forEach((lastStepBtn, index) => {
+    lastStepBtn.addEventListener('click', () => {
+        steps[index].classList.add('hidden')
+        stepsSelected[index].classList.remove('selected-steps')
+
+        if (steps[index - 1]) {
+            steps[index - 1].classList.remove('hidden')
+            stepsSelected[index - 1].classList.add('selected-steps')
+        }
+
+    })
+})
+
+/* Selected Plans */
+plansSelected.forEach(planSelected => {
+    planSelected.addEventListener('click', () => {
+        plansSelected.forEach(planSel => {
+            planSel.classList.remove('card-selected')
+        })
+        planSelected.classList.add('card-selected')
+
+    })
+});
+
+/* Monthly/Yearly Toggle */
 toggleInput.addEventListener('change', () => {
     plans.forEach(plan => plan.classList.remove('subscription-selected'))
 
@@ -163,9 +89,7 @@ toggleInput.addEventListener('change', () => {
 })
 
 
-/* ADD ONS */
-const addOnsSelected = document.querySelectorAll('.input__checkbox')
-
+/* Add Ons */
 addOnsSelected.forEach(addOnSelected => {
     addOnSelected.addEventListener('click', () => {
         if (addOnSelected.classList.contains('input__checkbox-selected')) {
